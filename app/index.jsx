42d6b4360e366fe36eb03be1615e81a2f61b9a3f
bombh <fabrice@bxxl.be>
2024-09-23 01:37:03 +0200

    Add images, vscode settings,...

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native"
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>FARMPROD</Text>
-    </View>
-  );
+   return (
+      <View
+         style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#FFF",
+         }}
+      >
+         <Image source={require("@/assets/images/logo_128.png")} />
+         <Text>FARMPROD</Text>
+      </View>
+   )
 }
