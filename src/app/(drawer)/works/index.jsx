eb30e30a409f5a1ed1d42ca580154c6b565940c2
bombh <fabrice@bxxl.be>
2024-09-23 19:55:19 +0200

    Add Works Screen

--- src/app/(drawer)/works/index.jsx
+++ src/app/(drawer)/works/index.jsx
@@ -1,16 +1,45 @@
-import { View, Text } from "react-native"
+import { View } from "react-native"
+import { FlashList } from "@shopify/flash-list"
+
+import useAPI from "@/src/hooks/useAPI"
+import ScreenTitle from "@/src/components/app/ScreenTitle"
+import WorkCard from "@/src/components/WorkCard"
 import HeaderDrawer from "@/src/layouts/HeaderDrawer"
 import Loading from "@/src/components/app/Loading"
 
-const Screen = () => {
+// const onEndReached = () => {
+//    console.log("Reached end of list")
+// }
+
+export default function Screen() {
+   const { data, isLoading, error } = useAPI("GET", "posts", "limit=100&include=tags")
+
    return (
       <>
          <HeaderDrawer />
-         <View className="flex-1 items-center justify-center bg-white">
-            <Loading label="Loading works" />
+         <View className="flex-1 px-5 bg-white">
+            {isLoading ? (
+               <Loading label="Loading Works" />
+            ) : (
+               <FlashList
+                  data={data.posts}
+                  renderItem={({ item, index }) => (
+                     <WorkCard
+                        {...item}
+                        index={index}
+                     />
+                  )}
+                  keyExtractor={(item) => item.id}
+                  estimatedItemSize={268}
+                  onEndReachedThreshold={0.5}
+                  ListHeaderComponent={<ScreenTitle title="Works" />}
+                  //onEndReached={onEndReached}
+                  // initialNumToRender={5}
+                  // maxToRenderPerBatch={5}
+                  // scrollEventThrottle={16}
+               />
+            )}
          </View>
       </>
    )
 }
-
-export default Screen
---
+++ src/components/WorkCard.jsx
@@ -0,0 +1,85 @@
+import { Image } from "expo-image"
+import { useRouter } from "expo-router"
+import { MotiView } from "moti"
+import { Pressable, Text, View } from "react-native"
+import { animations } from "@/src/constants"
+
+const placeholder = require("@/assets/images/placeholder.png")
+
+const suppressParenthesis = (string) => {
+   return string.replace("(", "").replace(")", "")
+}
+
+const WorkCard = ({ id, title, excerpt, feature_image, tags, index }) => {
+   const router = useRouter()
+
+   const img = feature_image.replace("/images/", "/images/size/w300/")
+   const imgHeader = feature_image.replace("/images/", "/images/size/w1000/")
+
+   // Bug in expo if parentheses are used in a string
+   const tagText = tags
+      .slice(2)
+      .map((tag) => suppressParenthesis(tag.name))
+      .join(" • ")
+
+   const showDetail = () => {
+      router.navigate({
+         pathname: `works/detail`,
+         params: {
+            id,
+            title: suppressParenthesis(title),
+            excerpt: suppressParenthesis(excerpt),
+            imgHeader,
+            tagText,
+         },
+      })
+   }
+
+   const anim = index < 4 ? animations.listItem(index) : {}
+
+   return (
+      <MotiView {...anim}>
+         <Pressable
+            className="mb-5 active:opacity-70"
+            onPress={showDetail}
+         >
+            <View className="relative">
+               <Image
+                  source={{ uri: img }}
+                  className="w-full h-40"
+                  placeholder={placeholder}
+                  placeholderContentFit="cover"
+                  transition={500}
+               />
+               <View className="absolute bg-black/70 bottom-0 w-full p-2">
+                  <Text
+                     numberOfLines={1}
+                     className="text-white font-semibold text-lg text-center"
+                  >
+                     {title.trim()}
+                  </Text>
+               </View>
+            </View>
+            <View className="p-2">
+               <Text
+                  numberOfLines={1}
+                  className="text-neutral-400 text-base text-center"
+               >
+                  {excerpt.trim()}
+               </Text>
+            </View>
+            <View className="border border-neutral-300 border-x-0 border-y-1">
+               <Text
+                  numberOfLines={1}
+                  className="text-xs text-center p-1"
+               >
+                  {tagText}
+               </Text>
+            </View>
+         </Pressable>
+      </MotiView>
+   )
+}
+
+export default WorkCard
+//export default memo(WorkCard, (prevProps, nextProps) => (prevProps.id === nextProps.id))
